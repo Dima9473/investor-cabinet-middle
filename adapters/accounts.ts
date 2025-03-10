6937710add20cd99651b812a5a2951d4593c662c
Dmitry Mednikov <m42106@mail.ru>
2025-03-10 13:26:49 +0300

    добавить типизацию

--- adapters/accounts.ts
+++ adapters/accounts.ts
@@ -1,6 +1,6 @@
-import { Accounts } from "../types/accounts";
+import { Accounts, AccountsDTO } from "../types/accounts";
 import { validateBankAccounts } from "../validation/account";
-export const adaptBankAccounts = (bankAccounts: any): Accounts | null => {
+export const adaptBankAccounts = (bankAccounts: AccountsDTO): Accounts | null => {
     if (!validateBankAccounts(bankAccounts)) return null
 
     const accounts: Accounts = bankAccounts
